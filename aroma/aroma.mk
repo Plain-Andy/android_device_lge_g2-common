dbe85fcf4a184927eb96cf35c5bde908385bc8d0
Snuzzo <jrracinfan@gmail.com>
2014-12-04 15:00:40 -0500

    Move from static to variables to incoporate aromazip function

--- aroma/aroma.mk
+++ aroma/aroma.mk
@@ -1,9 +1,13 @@
+AROMA_DEVICE_CONFIG := device/lge/g2-common/aroma/aroma-device-config
+AROMA_DEVICE_SCRIPT := device/lge/g2-common/aroma/aroma-device-script
+AROMA_DEVICE_FILES := device/lge/g2-common/aroma/aroma_device_files
+
 PRODUCT_COPY_FILES += \
-	device/lge/g2-common/aroma/aroma-device-config:aroma-device-config \
-	device/lge/g2-common/aroma/aroma-device-script:aroma-device-script \
-	device/lge/g2-common/aroma/aroma_device_files/deviceconfig.sh:aroma_device/deviceconfig.sh
+	$(AROMA_DEVICE_CONFIG):aroma-device-config \
+	$(AROMA_DEVICE_SCRIPT):aroma-device-script \
+	$(AROMA_DEVICE_FILES)/deviceconfig.sh:aroma_device/deviceconfig.sh
 
 #PRODUCT_COPY_FILES += \
-#	device/lge/g2-common/aroma/aroma_device_files/<SOURCE-FILE>:aroma_device/<DEST-FILE>
-## --- If any custom scripting please place in aroma_device dir and call on it in aroma-device-script
+#	$(AROMA_DEVICE_FILES)/<SOURCE-FILE>:aroma_device/<DEST-FILE>
+## --- If any custom scripting please place in aroma_device_files dir and call on it in aroma-device-script
 ## --- Squisher will retain any directory structure
